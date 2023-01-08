51f6d96bc694580402caa68e2504a5927a979a35
StoneDagher <115055489+Andrew-Dagher@users.noreply.github.com>
2023-01-08 14:01:29 -0500

    Update GUI.java
    finished the button functionality

--- src/personal_testing/GUI.java
+++ src/personal_testing/GUI.java
@@ -1,12 +1,26 @@
 package personal_testing;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class GUI {
+public class GUI implements ActionListener{
+	
+	private static JLabel userlabel;
+	private static JTextField userText;
+	private static JLabel passlabel;
+	private static JPasswordField passText;
+	private static JButton button;
+	private static JLabel success;
+	
+	
+	
 
 	public static void main(String[] args) {
 		
@@ -19,23 +33,47 @@ public class GUI {
 	
 		panel.setLayout(null);
 		
-		JLabel userlabel = new JLabel("User");//MAKING A LABEL
+		//username stuff
+		userlabel = new JLabel("User");//MAKING A LABEL
 		userlabel.setBounds(10,20,80,25);//location and size of panel
 		panel.add(userlabel);//add the label "user" to the panel
-		
-		JTextField userText = new JTextField();
+		userText = new JTextField();
 		userText.setBounds(100,20,165,25);
 		panel.add(userText);//add text box to the panel
-		JLabel passlabel = new JLabel("Password");
+		
+		//password stuff
+		passlabel = new JLabel("Password");
 		passlabel.setBounds(10,50,80,25);
 		panel.add(passlabel);
-		
-		JPasswordField passText = new JPasswordField();
+		passText = new JPasswordField();
 		passText.setBounds(100,50,165,25);
 		panel.add(passText);
 		
+		//button stuff
+		button = new JButton("Login");
+		button.setBounds(100,80,80,25);
+		button.addActionListener(new GUI());
+		panel.add(button);
+		
+		//
+		success = new JLabel("");
+		success.setBounds(10,110,300,25);
+		panel.add(success);
+		success.setText("");
+		
 		
 		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {//when button is pressed, this code runs
+		String user = userText.getText();
+		String password = passText.getText();
+		
+		if(user.toLowerCase().equals("andrew") && password.equals("HELLO")) {
+			success.setText("Login successful!");
+			
+		}else success.setText("Incorrect username or password, pleanse try again!");
 	}
 
 }
